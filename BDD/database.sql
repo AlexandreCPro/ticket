c93a281eeddae3033887d8fec9097dcc70cff4db
COUBERES Alexandre <alexandrecouberes@gmail.com>
2026-03-10 15:32:57 +0100

    update

--- BDD/database.sql
+++ BDD/database.sql
@@ -1,55 +1,16 @@
-CREATE DATABASE ticket;
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: ticket
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
 
-USE ticket;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_prenom VARCHAR(255) NOT NULL,
-    mot_de_passe VARCHAR(255) NOT NULL,
-    profil_id INT,
-    FOREIGN KEY (profil_id) REFERENCES profil(id)
-);
-
-CREATE TABLE profils (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    technicien VARCHAR(255) NOT NULL,
-    utilisateur VARCHAR(255) NOT NULL,
-    administrateur VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Agences (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_agence VARCHAR(255) NOT NULL,
-    code_postal INT(5) NOT NULL
-);
-
-CREATE TABLE assignation (
-    iduser INT,
-    idtickets INT,
-    FOREIGN KEY (iduser) REFERENCES users(id),
-    FOREIGN KEY (idtickets) REFERENCES tickets(id)
-);
-
-CREATE TABLE tickets (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    objet VARCHAR(255) NOT NULL,
-    contenu TEXT NOT NULL,
-    statut ENUM('ouvert', 'en cours', 'fermé') DEFAULT 'ouvert',
-    iduser INT,
-    idstatut INT,
-    idcategorie INT,
-    FOREIGN KEY (iduser) REFERENCES users(id),
-    FOREIGN KEY (idstatut) REFERENCES statut(id),
-    FOREIGN KEY (idcategorie) REFERENCES categories(id)
-);
-
-CREATE TABLE statut (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_statut VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE categories (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_categorie VARCHAR(255) NOT NULL
-);
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
--- save.bat
+++ save.bat
@@ -1,5 +1,5 @@
-C:\xampp\mysql\bin\mysqldump -uroot ticket>C:\Users\Rectorat Limoges\Desktop\htdocs\ticket\BDD\database.sql
-cd C:\Users\Rectorat Limoges\Desktop\htdocs\ticket
+C:\xampp\mysql\bin\mysqldump -uroot ticket>"C:\Users\Rectorat Limoges\Desktop\htdocs\ticket\BDD\database.sql"
+cd "C:\Users\Rectorat Limoges\Desktop\htdocs\ticket"
 git add .
 git commit -m "update"
 git push origin main
