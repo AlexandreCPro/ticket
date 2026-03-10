9e36cf1e3385bea420a86ac9125f3d4e1b0c460e
COUBERES Alexandre <alexandrecouberes@gmail.com>
2026-03-10 15:31:03 +0100

    update

--- BDD/database.sql
+++ BDD/database.sql
@@ -1,27 +1,55 @@
--- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: ticket
--- ------------------------------------------------------
--- Server version	10.4.32-MariaDB
+CREATE DATABASE ticket;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+USE ticket;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom_prenom VARCHAR(255) NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    profil_id INT,
+    FOREIGN KEY (profil_id) REFERENCES profil(id)
+);
 
--- Dump completed on 2026-01-20 14:47:33
+CREATE TABLE profils (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    technicien VARCHAR(255) NOT NULL,
+    utilisateur VARCHAR(255) NOT NULL,
+    administrateur VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Agences (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom_agence VARCHAR(255) NOT NULL,
+    code_postal INT(5) NOT NULL
+);
+
+CREATE TABLE assignation (
+    iduser INT,
+    idtickets INT,
+    FOREIGN KEY (iduser) REFERENCES users(id),
+    FOREIGN KEY (idtickets) REFERENCES tickets(id)
+);
+
+CREATE TABLE tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    objet VARCHAR(255) NOT NULL,
+    contenu TEXT NOT NULL,
+    statut ENUM('ouvert', 'en cours', 'fermé') DEFAULT 'ouvert',
+    iduser INT,
+    idstatut INT,
+    idcategorie INT,
+    FOREIGN KEY (iduser) REFERENCES users(id),
+    FOREIGN KEY (idstatut) REFERENCES statut(id),
+    FOREIGN KEY (idcategorie) REFERENCES categories(id)
+);
+
+CREATE TABLE statut (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom_statut VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom_categorie VARCHAR(255) NOT NULL
+);
+
